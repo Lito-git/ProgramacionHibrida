41a0652c3472a3cad5b591833cb93e9e5cfaeafc
Lito-git <tm.lito.salazar@gmail.com>
2025-04-25 07:27:15 -0400

    Subida Prueba U1 PH

---
+++ PRUEBA U1 Programacion Hibrida/manuel-salazar-pheva1/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'manuel.salazar.pheva1',
+  appName: 'manuel-salazar-pheva1',
+  webDir: 'www'
+};
+
+export default config;
